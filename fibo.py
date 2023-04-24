f6300079c4f24e01f395ea83a603ff15e67faac8
ejdamadam <247312@vut.cz>
2023-04-24 12:39:04 +0200

    fibb posloupnost

--- fibo.py
+++ fibo.py
@@ -1,7 +1,18 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
 
 def main():
+    i = int(input("Zadej který prvek Fibbonaciho posloupnosti chceš vypsat: "))
+    print(f"{i}. prvek Fibbonaciho posloupnosti je {fib(i)}.")
     pass
 
 
 if __name__ == "__main__":
     main()
+
